17edc0bbfe6d1d77638662e87e283aa05b93297b
sumon <hydravulgaris60@gmail.com>
2024-02-08 18:46:30 +0600

    console log fixed

--- index.js
+++ index.js
@@ -54,19 +54,7 @@ function deleteInvalids(array) {
   return onlyNumbers;
 }
 
-console.log(
-  deleteInvalids([
-    1,
-    null,
-    undefined,
-    18,
-    -19,
-    NaN,
-    "12",
-    [1, 2],
-    { ob: "lala" },
-  ])
-);
+console.log(deleteInvalids([1,null,undefined,18,-19,NaN,"12",[1, 2],{ ob: "lala" },]));
 console.log(deleteInvalids(["1", { num: 2 }, NaN]));
 console.log(deleteInvalids([1, 2, -3]));
 console.log(deleteInvalids({ num: [1, 2, 3] }));
